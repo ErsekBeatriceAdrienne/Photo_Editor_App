10b460c8fef66a8fcbde3f49041c94a45f489964
Bea <ersek.beatrice@student.ms.sapientia.ro>
2025-07-14 07:34:08 +0300

    profile page on destop

--- photo_editor_app/lib/frontend/pages/editing_video/edit_videos_page.dart
+++ photo_editor_app/lib/frontend/pages/editing_video/edit_videos_page.dart
@@ -1,11 +1,11 @@
 import 'dart:io';
+import 'dart:typed_data';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:photo_manager/photo_manager.dart';
 import 'package:provider/provider.dart';
-import '../../../l10n/app_localizations.dart';
-import 'dart:typed_data';
 
+import '../../../l10n/app_localizations.dart';
 import '../profile/accent_color_provider.dart';
 
 class EditVideosPage extends StatefulWidget {
@@ -25,45 +25,6 @@ class EditVideosPage extends StatefulWidget {
 }
 
 class _EditVideosPageState extends State<EditVideosPage> {
-  List<AssetEntity> _images = [];
-  late final VoidCallback onToggleTheme;
-  late final bool isDarkMode;
-  late final String? userId;
-
-  @override
-  void initState() {
-    super.initState();
-    if (Platform.isAndroid || Platform.isIOS) {
-      _loadGalleryImages();
-    }
-  }
-
-  Future<void> _loadGalleryImages() async {
-    final PermissionState ps = await PhotoManager.requestPermissionExtend();
-
-    if (!Platform.isAndroid && !Platform.isIOS) return;
-
-    if (ps.isAuth) {
-      await PhotoManager.clearFileCache();
-
-      List<AssetPathEntity> albums = await PhotoManager.getAssetPathList(
-        type: RequestType.image,
-      );
-
-      if (albums.isNotEmpty) {
-        final recent = albums.first;
-        final List<AssetEntity> media = await recent.getAssetListPaged(page: 0, size: 50);
-        setState(() {
-          _images = media;
-        });
-      }
-    } else {
-      setState(() {
-        _images = [];
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     final accentColor = Provider.of<AccentColorProvider>(context).accentColor;
@@ -72,119 +33,123 @@ class _EditVideosPageState extends State<EditVideosPage> {
             ? const Color(0xFF141414)
             : Colors.grey[100]);
 
+    final bool isMobile = Platform.isAndroid || Platform.isIOS;
+
     return Scaffold(
       backgroundColor: backgroundColor,
-      body: Stack(
-        children: [
-          CustomScrollView(
-            slivers: [
-              // Appbar
-              SliverAppBar(
-                backgroundColor: Colors.transparent,
-                surfaceTintColor: Colors.transparent,
-                shadowColor: Colors.transparent,
-                pinned: false,
-                expandedHeight: 50,
-                leadingWidth: 90,
-                automaticallyImplyLeading: false,
-                centerTitle: true,
-                floating: true,
-                snap: true,
-                // If this enabled than it's blurry
-                flexibleSpace: ClipRRect(
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 1.0, sigmaY: 1.0),
-                    child: Container(
-                      color: backgroundColor?.withOpacity(0.5),
-                    ),
-                  ),
-                ),
-                title: Text(
-                  AppLocalizations.of(context)!.videos_editing_page_title,
-                  style: TextStyle(
-                    fontSize: 20,
+      body: isMobile
+          ? _buildMobileBody(context, accentColor, backgroundColor)
+          : _buildDesktopBody(context, accentColor, backgroundColor),
+    );
+  }
+
+  // Mobilos nézet
+  Widget _buildMobileBody(
+      BuildContext context, Color accentColor, Color? backgroundColor) {
+    return CustomScrollView(
+      slivers: [
+        _buildSliverAppBarMobile(context, accentColor, backgroundColor),
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 10, 16, 8),
+                child: Text(
+                  AppLocalizations.of(context)!.videos_section_text,
+                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
                     fontWeight: FontWeight.bold,
-                    color: accentColor,
                   ),
                 ),
               ),
-
-              // Body
-              SliverToBoxAdapter(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(16, 10, 16, 8),
-                  child: Text(
-                    AppLocalizations.of(context)!.videos_section_text,
-                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-
-              SliverPadding(
+              Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 16),
-                sliver: SliverGrid(
-                  delegate: SliverChildListDelegate(
-                    [
-                      _buildSection(context, icon: Icons.camera, title: AppLocalizations.of(context)!.clips_button_text, onTap: () {}),
-                      _buildSection(context, icon: Icons.movie_creation_outlined, title: AppLocalizations.of(context)!.movies_button_text, onTap: () {}),
-                      _buildSection(context, icon: Icons.subtitles_outlined, title: AppLocalizations.of(context)!.subtitles_button_text, onTap: () {}),
-                    ],
-                  ),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 16,
-                    crossAxisSpacing: 16,
-                    childAspectRatio: 1.3,
-                  ),
+                child: GridView.count(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                  childAspectRatio: 1.3,
+                  children: [
+                    _buildSection(
+                      context,
+                      icon: Icons.video_call,
+                      title:
+                      AppLocalizations.of(context)!.clips_button_text,
+                      onTap: _handleVideoSelection,
+                    ),
+                    _buildSection(
+                      context,
+                      icon: Icons.movie_creation_outlined,
+                      title: AppLocalizations.of(context)!
+                          .movies_button_text,
+                      onTap: () {},
+                    ),
+                    _buildSection(
+                      context,
+                      icon: Icons.subtitles_outlined,
+                      title: AppLocalizations.of(context)!
+                          .subtitles_button_text,
+                      onTap: () {},
+                    ),
+                  ],
                 ),
               ),
-
-              /*
-              SliverPadding(
-                padding: const EdgeInsets.all(16.0),
-                sliver: _images.isNotEmpty
-                    ? SliverGrid(
-                  delegate: SliverChildBuilderDelegate(
-                        (context, index) {
-                      return FutureBuilder<Uint8List?>(
-                        future: _images[index].thumbnailDataWithSize(const ThumbnailSize(200, 200)),
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
-                            return Image.memory(snapshot.data!, fit: BoxFit.cover);
-                          } else {
-                            return const Center(child: CircularProgressIndicator());
-                          }
-                        },
-                      );
-                    },
-                    childCount: _images.length,
-                  ),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 4,
-                    mainAxisSpacing: 8,
-                    crossAxisSpacing: 8,
-                  ),
-                )
-                    : const SliverToBoxAdapter(child: SizedBox.shrink()),
-              ),
-
-               */
+              const SizedBox(height: 20),
             ],
           ),
-        ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSliverAppBarMobile(
+      BuildContext context, Color accentColor, Color? backgroundColor) {
+    return SliverAppBar(
+      backgroundColor: Colors.transparent,
+      surfaceTintColor: Colors.transparent,
+      shadowColor: Colors.transparent,
+      pinned: false,
+      expandedHeight: 50,
+      leadingWidth: 90,
+      automaticallyImplyLeading: false,
+      centerTitle: true,
+      floating: true,
+      snap: true,
+      flexibleSpace: ClipRRect(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 1.0, sigmaY: 1.0),
+          child: Container(
+            color: backgroundColor?.withOpacity(0.5),
+          ),
+        ),
+      ),
+      title: Text(
+        AppLocalizations.of(context)!.videos_editing_page_title,
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: accentColor,
+        ),
       ),
     );
   }
 
-  Widget _buildSection(
-      BuildContext context, {
-        required IconData icon,
+  Widget _buildDesktopBody(
+      BuildContext context, Color accentColor, Color? backgroundColor) {
+    return Center(
+      child: Text(
+        "Ez az asztali verzió video tartalma",
+        style: TextStyle(color: accentColor, fontSize: 20),
+      ),
+    );
+  }
+
+  Widget _buildSection(BuildContext context,
+      {required IconData icon,
         required String title,
-        required VoidCallback onTap,
-      })
-  {
+        required VoidCallback onTap}) {
     final accentColor = Provider.of<AccentColorProvider>(context).accentColor;
     final isDarkMode = Theme.of(context).brightness == Brightness.dark;
     final textColor = isDarkMode ? const Color(0xFF1E1E1E) : Colors.grey[100];
@@ -233,6 +198,94 @@ class _EditVideosPageState extends State<EditVideosPage> {
       ),
     );
   }
+
+  Future<void> _handleVideoSelection() async {
+    final PermissionState ps = await PhotoManager.requestPermissionExtend();
+    if (!ps.isAuth) return;
+
+    final List<AssetPathEntity> albums =
+    await PhotoManager.getAssetPathList(type: RequestType.video);
+    if (albums.isEmpty) return;
+
+    final List<AssetEntity> videos =
+    await albums.first.getAssetListPaged(page: 0, size: 100);
+    if (videos.isEmpty) return;
+
+    final selected = await showDialog<AssetEntity>(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: const Text("Válassz egy videót"),
+        content: SizedBox(
+          width: double.maxFinite,
+          height: 300,
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: 4,
+              mainAxisSpacing: 4,
+            ),
+            itemCount: videos.length,
+            itemBuilder: (context, index) {
+              return FutureBuilder<Uint8List?>(
+                future: videos[index]
+                    .thumbnailDataWithSize(const ThumbnailSize(200, 200)),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const SizedBox();
+                  }
+                  return GestureDetector(
+                    onTap: () => Navigator.pop(context, videos[index]),
+                    child: Image.memory(snapshot.data!, fit: BoxFit.cover),
+                  );
+                },
+              );
+            },
+          ),
+        ),
+      ),
+    );
+
+    if (selected == null) return;
+
+    final file = await selected.file;
+    if (file == null) return;
+
+    // TODO: Videó szerkesztőhöz való navigáció
+    // Pl. Navigator.push(...)
+
+    debugPrint('Kiválasztott videó path: ${file.path}');
+  }
 }
 
 
+
+/*
+              SliverPadding(
+                padding: const EdgeInsets.all(16.0),
+                sliver: _images.isNotEmpty
+                    ? SliverGrid(
+                  delegate: SliverChildBuilderDelegate(
+                        (context, index) {
+                      return FutureBuilder<Uint8List?>(
+                        future: _images[index].thumbnailDataWithSize(const ThumbnailSize(200, 200)),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
+                            return Image.memory(snapshot.data!, fit: BoxFit.cover);
+                          } else {
+                            return const Center(child: CircularProgressIndicator());
+                          }
+                        },
+                      );
+                    },
+                    childCount: _images.length,
+                  ),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    mainAxisSpacing: 8,
+                    crossAxisSpacing: 8,
+                  ),
+                )
+                    : const SliverToBoxAdapter(child: SizedBox.shrink()),
+              ),
+
+               */
