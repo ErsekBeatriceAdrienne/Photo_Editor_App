941fdeade2597c8adbae9049c492e422829c9daf
Bea <ersek.beatrice@student.ms.sapientia.ro>
2025-07-16 10:04:41 +0300

    editing image main page

--- photo_editor_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ photo_editor_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
 import file_selector_macos
 import macos_window_utils
 import photo_manager
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   MacOSWindowUtilsPlugin.register(with: registry.registrar(forPlugin: "MacOSWindowUtilsPlugin"))
   PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
